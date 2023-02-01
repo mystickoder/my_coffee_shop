d62356defe3b77534661aca7dd1aecadcf9554bf
mystickoder <89115482+mystickoder@users.noreply.github.com>
2023-01-31 22:57:34 -0500

    My Coffee Shop

---
+++ my_coffee_shop.py
@@ -0,0 +1,14 @@
+print("Welcomes to Kathy's Coffee House")
+
+name = input("What is your name? \n")
+
+print("Hello " + name + ", thank you so much for coming today. \n")
+
+menu = ("Black Coffee", "Espresso", "Latte", "Cappucino")
+
+print(name + " , what  would you like from our menu today? Here is what we are serving. \n")
+
+order = input()
+
+print("Sounds good " +name + ", we'll have that " + order + " ready for you in a moment.")
+
